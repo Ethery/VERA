04be277eef3ccbcc353d9ed47d9a0b8fb7cd018e
Ethery <loic.meunier74@gmail.com>
2024-05-31 13:55:04 +0200

    Added Ai base entity property and nav mesh plugin.

--- Unity/Assets/Scripts/Game/Entities/Character.cs
+++ Unity/Assets/Scripts/Game/Entities/Character.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityTools.Systems.Inputs;
 
@@ -13,12 +10,14 @@ public class Character : Entity
 	{
 		InputManager.RegisterInput(m_moveInput, new InputManager.InputEvent(OnMoveInput_Performed, InputActionPhase.Performed), true);
 		InputManager.RegisterInput(m_moveInput, new InputManager.InputEvent(OnMoveInput_Canceled, InputActionPhase.Canceled), true);
+		InputManager.RegisterInput(m_sprintInput, new InputManager.InputEvent(OnSprintInput_Performed, InputActionPhase.Performed), true);
+		InputManager.RegisterInput(m_sprintInput, new InputManager.InputEvent(OnSprintInput_Canceled, InputActionPhase.Canceled), true);
 	}
 
 	private void LateUpdate()
 	{
 		m_moveDirection.y = GetComponent<Rigidbody>().velocity.y;
-		GetComponent<Rigidbody>().velocity = m_moveDirection;
+		GetComponent<Rigidbody>().velocity = m_moveDirection * (m_sprinting ? m_sprintSpeed : m_speed);
 		if(m_moveDirection != Vector3.zero)
 		{
 			transform.forward = m_moveDirection;
@@ -33,8 +32,8 @@ public class Character : Entity
 
 	public void OnMoveInput_Performed(InputAction inputAction)
 	{
-		Vector2 inputDirection = inputAction.ReadValue<Vector2>();
-		m_moveDirection = m_speed * new Vector3(inputDirection.x, 0, inputDirection.y);
+		Vector2 inputDirection = inputAction.ReadValue<Vector2>().normalized;
+		m_moveDirection = new Vector3(inputDirection.x, 0, inputDirection.y);
 	}
 
 	public void OnMoveInput_Canceled(InputAction inputAction)
@@ -42,12 +41,28 @@ public class Character : Entity
 		m_moveDirection = Vector3.zero;
 	}
 
+	public void OnSprintInput_Performed(InputAction inputAction)
+	{
+		m_sprinting = true;
+	}
+
+	public void OnSprintInput_Canceled(InputAction inputAction)
+	{
+		m_sprinting = false;
+	}
+
 	[NonSerialized]
 	private Vector3 m_moveDirection;
+	[NonSerialized]
+	private bool m_sprinting;
 
 	[SerializeField]
 	private float m_speed = 1f;
+	[SerializeField]
+	private float m_sprintSpeed = 2f;
 
 	[SerializeField]
 	private InputManager.Input m_moveInput;
+	[SerializeField]
+	private InputManager.Input m_sprintInput;
 }
