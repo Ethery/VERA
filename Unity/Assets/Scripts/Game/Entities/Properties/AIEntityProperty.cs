901945f27b095ed9edb8f7176b805546958e4469
Ethery <loic.meunier74@gmail.com>
2024-06-02 00:18:19 +0200

    Base movement of AI

--- Unity/Assets/Scripts/Game/Entities/Properties/AIEntityProperty.cs
+++ Unity/Assets/Scripts/Game/Entities/Properties/AIEntityProperty.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class AiEntityProperty : EntityProperty
 {
+	private void Update()
+	{
+		MoveTo(m_debugTransform.position);
+	}
 
+	public void MoveTo(Vector3 targetPosition)
+	{
+		GetComponent<NavMeshAgent>().SetDestination(targetPosition);
+	}
+
+	[SerializeField]
+	private Transform m_debugTransform;
+
+	private Vector3 m_targetPosition;
 }
