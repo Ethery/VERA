054de6def87df4ee975d68d6aea51cba43aadcb9
Ethery <loic.meunier74@gmail.com>
2024-08-08 23:24:06 +0200

    Bases for Cameramovement.

--- Unity/Packages/com.ethery.unitytools/System/CameraManager.cs
+++ Unity/Packages/com.ethery.unitytools/System/CameraManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -29,12 +30,20 @@ public class CameraManager : Singleton<CameraManager>
 									, register);
 
 		InputManager.RegisterInput(m_cameraSpeedControlInput
-									, new InputManager.InputEvent(OnCameraSpeedControl, InputActionPhase.Performed)
+									, new InputManager.InputEvent(OnCameraSpeedControl_Performed, InputActionPhase.Performed)
+									, register);
+
+		InputManager.RegisterInput(m_cameraDragInput
+                                    , new InputManager.InputEvent(OnCameraDrag_Performed, InputActionPhase.Performed)
 									, register);
 	}
 
+    private void OnCameraDrag_Performed(InputAction action)
+    {
+        throw new NotImplementedException();
+    }
 
-	public void OnCameraMovement(InputAction action)
+    public void OnCameraMovement(InputAction action)
 	{
 		wantedMovement = action.ReadValue<Vector2>();
 	}
@@ -48,7 +57,7 @@ public class CameraManager : Singleton<CameraManager>
 		speedUp = action.ReadValue<float>() > 0f;
 	}
 
-	public void OnCameraSpeedControl(InputAction action)
+	public void OnCameraSpeedControl_Performed(InputAction action)
 	{
 		float actionValue = action.ReadValue<float>();
 		actionValue = Mathf.Clamp(actionValue, -0.2f, 0.2f);
@@ -58,14 +67,23 @@ public class CameraManager : Singleton<CameraManager>
 
 	private void FixedUpdate()
 	{
-		if(wantedMovement != Vector2.zero)
+		if (m_currentCamera == null)
+			m_currentCamera = Camera.main;
+		if (m_currentCamera != null)
 		{
-			Vector3 newPosition = transform.position;
+			if (wantedMovement != Vector2.zero)
+			{
+				Vector3 newPosition = m_currentCamera.transform.position;
 
-			newPosition.x += wantedMovement.x * m_cameraBaseSpeed * Time.deltaTime * (speedUp ? m_cameraSpeedMultiplier : 1);
-			newPosition.z += wantedMovement.y * m_cameraBaseSpeed * Time.deltaTime * (speedUp ? m_cameraSpeedMultiplier : 1);
+				newPosition.x += wantedMovement.x * m_cameraBaseSpeed * Time.deltaTime * (speedUp ? m_cameraSpeedMultiplier : 1);
+				newPosition.z += wantedMovement.y * m_cameraBaseSpeed * Time.deltaTime * (speedUp ? m_cameraSpeedMultiplier : 1);
 
-			transform.position = newPosition;
+				m_currentCamera.transform.position = newPosition;
+			}
+		}
+		else
+		{
+			Debug.Log($"No camera");
 		}
 	}
 
@@ -77,21 +95,27 @@ public class CameraManager : Singleton<CameraManager>
 	private Vector2 wantedMovement = Vector2.zero;
 	private bool speedUp = false;
 
-	[SerializeField]
+	[SerializeField] 
 	private InputManager.Input m_cameraMoveInput;
-	[SerializeField]
+	[SerializeField] 
 	private InputManager.Input m_cameraSpeedUpInput;
-	[SerializeField]
+	[SerializeField] 
 	private InputManager.Input m_cameraSpeedControlInput;
+	[SerializeField]
+	private InputManager.Input m_cameraDragInput;
 
-	[SerializeField]
+	[SerializeField] 
 	private float m_cameraMaxSpeed = 10.0f;
-	[SerializeField]
+	[SerializeField] 
 	private float m_cameraMinSpeed = 0.5f;
 
-	[SerializeField]
+	[SerializeField] 
 	private float m_cameraBaseSpeed = 5.0f;
 
-	[SerializeField]
+	[SerializeField] 
 	private float m_cameraSpeedMultiplier = 2.0f;
+
+	[NonSerialized]
+	private Camera m_currentCamera = null;
+
 }
